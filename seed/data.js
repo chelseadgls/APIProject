0912358b9ee24eb61d83ec4808437dc57d42d002
chelseadgls <chelseamdouglas@gmail.com>
2022-10-18 13:54:34 -0400

    data inserted

--- seed/data.js
+++ seed/data.js
@@ -4,51 +4,46 @@ import teamData from './teamdata.json' assert { type: "json" }
 import Player from '../models/Player.js' 
 import Team from '../models/Team.js'
 
-
 // console.log(playerData);
 // console.log(teamData);
-
+  
 let structuredPlayerData = playerData.map(
-  ({ _id, name, headOfHouse, houseGhost, founder, school, members, values, colors }) => {
+  ({ id, first_name, last_name, position, height_feet, height_inches, weight_pounds, team }) => {
     return {
-      _id,
-      name,
-      headOfHouse,
-      houseGhost,
-      founder,
-      school,
-      members,
-      values,
-      colors,
+      id,
+      first_name,
+      last_name,
+      position,
+      height_feet,
+      height_inches,
+      weight_pounds,
+      team
     };
   }
 );
-
+  
 let structuredTeamData = teamData.map(
-  ({ _id, name, house, school, ministryOfMagic, orderOfThePhoenix, dumbledoresArmy, deathEater, bloodStatus, species }) => {
+  ({ id, abbreviation, city, conference, division, full_name, name }) => {
     return {
-      _id,
-      name,
-      house,
-      school,
-      ministryOfMagic,
-      orderOfThePhoenix,
-      dumbledoresArmy,
-      deathEater,
-      bloodStatus,
-      species,
+      id,
+      abbreviation,
+      city,
+      conference,
+      division,
+      full_name,
+      name
     };
   }
 );
 
-//insert data
-// let insertData = async () => {
-//   await Player.deleteMany({});
-//   await Team.deleteMany({});
-//   await Player.create(structuredPlayerData);
-//   await Team.create(structuredTeamData);
-//   console.log('Teams and Players inserted');
-//   // mongoose.disconnect();
-// };
+// insert data
+let insertData = async () => {
+  await Player.deleteMany({});
+  await Team.deleteMany({});
+  await Player.create(structuredPlayerData);
+  await Team.create(structuredTeamData);
+  console.log('Teams and Players inserted');
+  // mongoose.disconnect();
+};
 
-// insertData();
+insertData();
