34daeb8665ea79d9b4f534ba67526ba46f5ed6c0
chelseadgls <chelseamdouglas@gmail.com>
2022-10-18 16:28:12 -0400

    new models seeded

--- seed/api.js
+++ seed/api.js
@@ -13,10 +13,7 @@ const gameURL = 'https://www.balldontlie.io/api/v1/games'
 //Stat endpoint
 const statURL = 'https://www.balldontlie.io/api/v1/stats'
 
-//Average endpoint
-const averageURL = 'https://www.balldontlie.io/api/v1/season_averages'
-
-// API Call
+// Player
 async function fetchPlayer() {
   let response = await axios.get(playerURL);
   console.log(response.data)
@@ -32,17 +29,50 @@ async function fetchPlayer() {
 
 fetchPlayer();
 
-// async function fetchTeam() {
-//   let response = await axios.get(teamURL);
-//   console.log(response.data)
-//   const data = response.data;
-//   const teamData = JSON.stringify(data);
-//   fs.writeFile('teamdata.json', teamData, err => {
-//     if (err) {
-//       throw err
-//     }
-//     console.log('Team data is saved');
-//   });
-// };
+// Team
+async function fetchTeam() {
+  let response = await axios.get(teamURL);
+  console.log(response.data)
+  const data = response.data;
+  const teamData = JSON.stringify(data);
+  fs.writeFile('teamdata.json', teamData, err => {
+    if (err) {
+      throw err
+    }
+    console.log('Team data is saved');
+  });
+};
 
-// fetchTeam();
+fetchTeam();
+
+// Game
+async function fetchGame() {
+  let response = await axios.get(gameURL);
+  console.log(response.data)
+  const data = response.data;
+  const gameData = JSON.stringify(data);
+  fs.writeFile('gamedata.json', gameData, err => {
+    if (err) {
+      throw err
+    }
+    console.log('Game data is saved');
+  });
+};
+
+fetchGame();
+
+// Stat
+async function fetchStat() {
+  let response = await axios.get(statURL);
+  console.log(response.data)
+  const data = response.data;
+  const statData = JSON.stringify(data);
+  fs.writeFile('statdata.json', statData, err => {
+    if (err) {
+      throw err
+    }
+    console.log('Stat data is saved');
+  });
+};
+
+fetchStat();
--- seed/data.js
+++ seed/data.js
@@ -1,12 +1,17 @@
 import mongoose from '../db/connection.js';
 import playerData from './playerdata.json' assert { type: "json" }
 import teamData from './teamdata.json' assert { type: "json" }
+import gameData from './gamedata.json' assert { type: "json" }
+import statData from './statdata.json' assert { type: "json" }
 import Player from '../models/Player.js' 
 import Team from '../models/Team.js'
+import Game from '../models/Game.js'
+import Stat from '../models/Stat.js'
 
 // console.log(playerData);
 // console.log(teamData);
   
+// Player
 let structuredPlayerData = playerData.map(
   ({ id, first_name, last_name, position, team }) => {
     return {
@@ -19,6 +24,7 @@ let structuredPlayerData = playerData.map(
   }
 );
   
+// Team
 let structuredTeamData = teamData.map(
   ({ id, abbreviation, city, conference, division, full_name, name }) => {
     return {
@@ -33,13 +39,67 @@ let structuredTeamData = teamData.map(
   }
 );
 
+// Game
+let structuredGameData = gameData.map(
+  ({ id, date, home_team_score, visitor_team_score, season, period, status, time, postseason, home_team, visitor_team }) => {
+    return {
+      id,
+      date,
+      home_team_score,
+      visitor_team_score,
+      season,
+      period,
+      status,
+      time,
+      postseason,
+      home_team,
+      visitor_team
+    };
+  }
+);
+
+// Stat
+let structuredStatData = statData.map(
+  ({ id, ast, blk, dreb, fg3_pct, fg3a, fg3m, fga, fgm, ft_pct, fta, ftm, game, min, oreb, pf, player, pts, reb, stl, team, turnover }) => {
+    return {
+      id,
+      ast,
+      blk,
+      dreb,
+      fg3_pct,
+      fg3a,
+      fg3m,
+      fg3_pct,
+      fga,
+      fgm,
+      ft_pct,
+      fta,
+      ftm,
+      game,
+      min,
+      oreb,
+      pf,
+      player,
+      pts,
+      reb,
+      stl,
+      team,
+      turnover
+    };
+  }
+);
+
 // insert data
 let insertData = async () => {
   await Player.deleteMany({});
   await Team.deleteMany({});
+  await Game.deleteMany({});
+  await Stat.deleteMany({});
   await Player.create(structuredPlayerData);
   await Team.create(structuredTeamData);
-  console.log('Teams and Players inserted');
+  await Game.create(structuredGameData);
+  await Stat.create(structuredStatData);
+  console.log('Data inserted');
   // mongoose.disconnect();
 };
 
