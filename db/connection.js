5af2c380b6308054191bc5f29ff04ecb5f5a0ab4
chelseadgls <chelseamdouglas@gmail.com>
2022-10-18 12:06:28 -0400

    server up and running

--- db/connection.js
+++ db/connection.js
@@ -1,20 +1,20 @@
 import mongoose from 'mongoose';
 import chalk from 'chalk';
 
-mongoose.set('returnOriginal', false);
+mongoose.set("returnOriginal", false);
 
 mongoose
-  .connect('mongodb://127.0.0.1/27017/balldontlie')
+  .connect("mongodb://127.0.0.1:27017/nba-data")
   .catch((error) => {
     console.log(`Error connecting to MongoDB: ${error.message}`);
   });
 
-  mongoose.connection.on("disconnected", () => {
-    console.log(chalk.bold("Disconnected from MongoDB!"));
-  });
-    
-  mongoose.connection.on("error", (error) => {
-    console.log(chalk.red(`MongoDB connection error: ${error}`));
-  });
+mongoose.connection.on("disconnected", () => {
+  console.log(chalk.bold("Disconnected from MongoDB!"));
+});
   
-  export default mongoose.connection
+mongoose.connection.on("error", (error) => {
+  console.log(chalk.red(`MongoDB connection error: ${error}`));
+});
+
+export default mongoose.connection
